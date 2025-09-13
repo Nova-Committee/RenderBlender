6b1d364e94de8cdf483acb06cc918865b8341652
cu6 <3077476144@qq.com>
2025-09-13 21:08:01 +0800

    FixRender

--- src/main/java/committee/nova/mods/renderblender/client/model/CosmicBakeModel.java
+++ src/main/java/committee/nova/mods/renderblender/client/model/CosmicBakeModel.java
@@ -16,6 +16,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
