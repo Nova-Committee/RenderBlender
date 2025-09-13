2b4f90fd68f3519904dc71f77f688beef3c08ec9
cu6 <3077476144@qq.com>
2025-09-13 12:07:53 +0800

    Initial commit

---
+++ src/main/java/committee/nova/mods/renderblender/RenderBlenderLib.java
@@ -0,0 +1,42 @@
+package committee.nova.mods.renderblender;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(RenderBlenderLib.MOD_ID)
+public class RenderBlenderLib
+{
+
+    public static final String MOD_ID = "renderblender";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static ResourceLocation rl(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+    public RenderBlenderLib()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+}
